935e6fa0b6d77b69f30280e73fe69fa39d66ca77
Josh Meek <joshmeek18@gmail.com>
2025-03-07 10:28:27 -0500

    Fix eslint errors

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  metadataBase: new URL('https://isidor.onrender.com'),
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
