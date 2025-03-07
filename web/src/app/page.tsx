348218bd915e715f68feb697c48c64ece31cac38
Josh Meek <joshmeek18@gmail.com>
2025-03-07 10:20:09 -0500

    Fix eslint errors

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '@/components/Hero'
-import Features from '@/components/Features'
-import CTA from '@/components/CTA'
+// import Features from '@/components/Features'
+// import CTA from '@/components/CTA'
 
 export default function Home() {
   return (
